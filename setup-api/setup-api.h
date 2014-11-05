fd52e9aac08a7d7ee01563e6846ad6406a5b7c73
Oleg Kolosov <bazurbat@gmail.com>
2014-11-05 23:56:04 +0300

    Added setup-api from chicken-5 as an egg

---
+++ setup-api/setup-api.h
@@ -0,0 +1,20 @@
+#ifndef SETUP_API_H
+#define SETUP_API_H
+
+#define C_INSTALL_BIN_HOME ""
+#define C_TARGET_CC ""
+#define C_TARGET_CFLAGS ""
+#define C_TARGET_CXX ""
+#define C_TARGET_LIB_HOME ""
+#define C_TARGET_MORE_LIBS ""
+#define C_WINDOWS_SHELL ""
+
+#define C_CHICKEN_BUG_PROGRAM ""
+#define C_CHICKEN_INSTALL_PROGRAM ""
+#define C_CHICKEN_PROGRAM ""
+#define C_CHICKEN_STATUS_PROGRAM ""
+#define C_CHICKEN_UNINSTALL_PROGRAM ""
+#define C_CSC_PROGRAM ""
+#define C_CSI_PROGRAM ""
+
+#endif
